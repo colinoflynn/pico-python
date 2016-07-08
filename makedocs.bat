f1f9b7958f306cb344905a67b86894eda5b07c4e
Florian LB <florian.s.lebourdais@gmail.com>
2016-07-08 15:02:15 +0200

    add doc generation for other versions

--- makedocs.bat
+++ makedocs.bat
@@ -4,6 +4,7 @@ REM Otherwise can comment out...
 set PYTHONBIN=c:\Python27-32bit
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.picobase
+%PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps3000a
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps5000a
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps6000
 move *.html doc\.
