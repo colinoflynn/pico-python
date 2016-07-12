f9e0a8bf3bc09caf5cf0af469abd9d1bbfb12d35
Adam McCaughan <amccaugh@gmail.com>
2016-07-12 14:26:13 -0600

    Initial commit adding support for 2000A and 3000 series

--- makedocs.bat
+++ makedocs.bat
@@ -5,6 +5,8 @@ set PYTHONBIN=c:\Python27-32bit
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.picobase
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps2000
+%PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps2000a
+%PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps3000
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps3000a
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps4000
 %PYTHONBIN%\python.exe %PYTHONBIN%\Lib\pydoc.py -w picoscope.ps5000a
