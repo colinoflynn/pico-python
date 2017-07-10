19bb90d551ec9025d2003dca0c42bc15bfa95de6
Mark Harfouche <mark.harfouche@gmail.com>
2017-07-10 10:50:27 -0700

    Update ps2000a.py with mac support

--- picoscope/ps2000a.py
+++ picoscope/ps2000a.py
@@ -139,9 +139,12 @@ class PS2000a(_PicoscopeBase):
 
     def __init__(self, serialNumber=None, connect=True):
         """Load DLL etc"""
-        if platform.system() == 'Linux':
+        if platform.system() == 'Linux' :
             from ctypes import cdll
             self.lib = cdll.LoadLibrary("lib" + self.LIBNAME + ".so")
+        elif platform.system() == 'Darwin' :
+            from ctypes import cdll
+            self.lib = cdll.LoadLibrary("lib" + self.LIBNAME + ".dylib")
         else:
             from ctypes import windll
             self.lib = windll.LoadLibrary(self.LIBNAME + ".dll")
