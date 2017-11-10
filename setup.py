d17ef1c56549e446fca476072bf956a8475620a1
Nick Kirkby <nkirkby.nk@gmail.com>
2017-11-10 13:55:38 -0800

    Bumping version to 0.6.4
    issue #101 make version number a string

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name = 'picoscope',
-    version = '0.6.3',
+    version = '0.6.4',
     description = "Unofficial python wrapper for the PicoScope devices.",
     author = "Colin O'Flynn, Mark Harfouche",
     author_email = 'coflynn@newae.com, mark.harfouche@gmail.com',
     license = 'BSD',
     url = 'https://github.com/colinoflynn/pico-python/',
-    download_url='https://github.com/colinoflynn/picoscope/tarball/0.6.3',
+    download_url='https://github.com/colinoflynn/picoscope/tarball/0.6.4',
     packages = ['picoscope'],
     # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
     classifiers = [
