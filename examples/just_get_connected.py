e030ad6b102aeb162bfdf90ae3771d7ffa7b55b9
John McMaster <johndmcmaster@gmail.com>
2018-07-19 17:42:01 -0700

    examples: __main__ guards on all files
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- examples/just_get_connected.py
+++ examples/just_get_connected.py
@@ -7,41 +7,41 @@ if platform.system() == "Windows":
 elif platform.system() == "Linux":
     lib = cdll.LoadLibrary("libps3000a.so")
 
+if __name__ == "__main__":
+    # Enumerating units
+    print("Enumerating units...\n\n")
 
-# Enumerating units
-print("Enumerating units...\n\n")
+    count = c_short()
+    serials = (c_char * 100)()
+    serialLth = c_short()
+    lib.ps3000aEnumerateUnits(byref(count), byref(serials), byref(serialLth))
 
-count = c_short()
-serials = (c_char * 100)()
-serialLth = c_short()
-lib.ps3000aEnumerateUnits(byref(count), byref(serials), byref(serialLth))
+    print("count:" + str(count))
+    print("serial numbers: " + str(serials[0:100]))
+    print("serial string length: " + str(serialLth))
 
-print("count:" + str(count))
-print("serial numbers: " + str(serials[0:100]))
-print("serial string length: " + str(serialLth))
+    # try connecting
+    serial_len = serialLth.value
+    serial_num = serials[0:serial_len]
+    print("\n\nAttempting to connect to" + str(serial_num) + "\n\n")
+    handle = c_short()
+    s = create_string_buffer(serial_num)
 
-# try connecting
-serial_len = serialLth.value
-serial_num = serials[0:serial_len]
-print("\n\nAttempting to connect to" + str(serial_num) + "\n\n")
-handle = c_short()
-s = create_string_buffer(serial_num)
+    m = lib.ps3000aOpenUnit(byref(handle), byref(s))
+    print("Result code:" + str(m))
+    print("Scope handle:" + str(handle.value))
 
-m = lib.ps3000aOpenUnit(byref(handle), byref(s))
-print("Result code:" + str(m))
-print("Scope handle:" + str(handle.value))
-
-try:
-    # flash led
-    print("\n\nFlashing LED...\n\n")
-    start = c_short(10)
-    lib.ps3000aFlashLed(handle, start)
-    time.sleep(5)
-except: # noqa
-    print("led wouldn't flash")
-    pass
-# close unit
-print("\n\nClosing scope...\n\n")
-m = lib.ps3000aCloseUnit(handle)
-if m == 0:
-    print("Unit" + str(serial_num) + "shut down.")
+    try:
+        # flash led
+        print("\n\nFlashing LED...\n\n")
+        start = c_short(10)
+        lib.ps3000aFlashLed(handle, start)
+        time.sleep(5)
+    except: # noqa
+        print("led wouldn't flash")
+        pass
+    # close unit
+    print("\n\nClosing scope...\n\n")
+    m = lib.ps3000aCloseUnit(handle)
+    if m == 0:
+        print("Unit" + str(serial_num) + "shut down.")
