bc2c9e2e37a00c001079030afb2fd814afbeb958
John Mcmaster <JohnDMcMaster@gmail.com>
2018-07-21 19:44:30 -0700

    just_get_connected: guard windows only import (#136)
    * just_get_connected: guard windows only import
    
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- examples/just_get_connected.py
+++ examples/just_get_connected.py
@@ -1,8 +1,10 @@
-from ctypes import c_short, byref, cdll, windll, c_char, create_string_buffer
+from ctypes import c_short, byref, cdll, c_char, create_string_buffer
 import time
 import platform
 
 if platform.system() == "Windows":
+    from ctypes import windll
+
     lib = windll.LoadLibrary("ps3000a.dll")
 elif platform.system() == "Linux":
     lib = cdll.LoadLibrary("libps3000a.so")
@@ -37,7 +39,7 @@ try:
     start = c_short(10)
     lib.ps3000aFlashLed(handle, start)
     time.sleep(5)
-except: # noqa
+except:  # noqa
     print("led wouldn't flash")
     pass
 # close unit
