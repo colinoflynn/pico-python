76c63d4989746ee020c160963b922a5bb8ad511d
elechapt <eliottlechapt@yahoo.fr>
2021-05-25 16:41:42 +0200

    Fixing issue #169

--- picoscope/ps5000a.py
+++ picoscope/ps5000a.py
@@ -143,7 +143,9 @@ class PS5000a(_PicoscopeBase):
     def _lowLevelOpenUnit(self, sn):
         c_handle = c_int16()
         if sn is not None:
-            serialNullTermStr = create_string_buffer((bytes(sn, encoding='utf-8'))
+            serialNullTermStr = create_string_buffer(
+                    bytes(sn, encoding='utf-8')
+                    )
         else:
             serialNullTermStr = None
         # Passing None is the same as passing NULL
