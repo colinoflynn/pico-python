9386089f0ca221b08eb203583292e7a9f4c50dca
Benedikt Moneke <67148916+bmoneke@users.noreply.github.com>
2022-01-26 13:51:06 +0100

    Tests improved after trials with a ps4000a series.

--- examples/test_ps6000a.py
+++ examples/test_ps6000a.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Testing the ps6000a Series.
+Testing the ps6000a series software-device interaction.
+======================================================
+
+This file provides tests in order to verify that the software does with the
+oscilloscope, what it is supposed to do. Additionally the tests serve as
+examples on how to do certain tasks.
+
+Usage
+-----
+
+- Run this file in order to execute all software tests on your device.
+- Import this file and execute the tests you want with your already opened
+  oscilloscope device.
 
 Created on Tue Jan 25 12:20:14 2022 by Benedikt Moneke
 """
@@ -12,15 +24,15 @@ from picoscope import ps6000a
 import time
 
 
-def test_general_unit_calls():
-    assert ps.openUnitProgress()
+def test_general_unit_calls(ps):
+    """Test general unit calls"""
     ps.flashLed()
     print(ps.getAllUnitInfo())
-    assert not ps.ping()
+    assert not ps.ping(), "Ping failed."
     print("General unit calls test passed.")
 
 
-def test_timebase():
+def test_timebase(ps):
     """Test the timebase methods."""
     # Preparation
     ps.memorySegments(1)
@@ -32,7 +44,8 @@ def test_timebase():
             (10e-9, 5),
             (15e-9, 6))
     for (time, timebase) in data:
-        assert ps.getTimeBaseNum(time) == timebase
+        text = f"Time {time} does not give timebase {timebase}"
+        assert ps.getTimeBaseNum(time) == timebase, text
     data = ((200e-12, 0),
             (800e-12, 2),
             (3.2e-9, 4),
@@ -41,23 +54,25 @@ def test_timebase():
             (3.84e-8, 10),
             (6.144e-7, 100))
     for (time, timebase) in data:
-        assert ps.getTimestepFromTimebase(timebase) == time
+        text = f"time {time} does not fit timebase {timebase}."
+        assert ps.getTimestepFromTimebase(timebase) == time, "Timestep " + text
         timestep, _ = ps._lowLevelGetTimebase(timebase, 10, None, 0)
-        assert timestep == time
+        assert timestep == time, f"LowLevel: {timestep} != {time}"
     print("Timebase test passed.")
 
 
-def test_deviceResolution():
+def test_deviceResolution(ps):
     """Test setting/getting device resolution, including ADC limits."""
     ps.setResolution("12")
-    assert ps.resolution == "12"
-    assert ps.getResolution() == "12"
-    assert ps.MIN_VALUE == -32736
-    assert ps.MAX_VALUE == 32736
+    assert ps.resolution == "12", "Picoscope variable was not set."
+    assert ps.getResolution() == "12", "Picoscope resolution is wrong."
+    assert ps.MIN_VALUE == -32736, "Minimum adc value is wrong."
+    assert ps.MAX_VALUE == 32736, "Maximum adc value is wrong."
     print("Device resolution test passed.")
 
 
-def test_rapid_block_mode(n_captures=100,
+def test_rapid_block_mode(ps,
+                          n_captures=100,
                           sample_interval=100e-9,  # 100 ns
                           sample_duration=2e-3,  # 1 ms
                           ):
@@ -108,36 +123,52 @@ def data_ready(handle, status, noOfSamples, overflow, pParameter=None):
 
 
 def test_read_async(handle=None, status=0, pParameter=None):
-    """Test reading data asynchronously."""
+    """
+    Test reading data asynchronously.
+
+    If you call it manually instead of using it as a callback, use pParameter
+    for handing over the picoscope instance.
+    """
+    if pParameter is not None:
+        psg = pParameter
     if status == 0:
         print("Block is ready and can be read.")
+        # config is a global variable written by the caller.
         channel, numSamples = config
+        # Define data globally for data_ready.
         global data
         data = np.empty(numSamples, dtype=np.int16)
-        ps._lowLevelSetDataBuffer(channel, data, 0, 0)
-        ps._lowLevelGetValuesAsync(numSamples, 0, 1, 0, 0, data_ready)
-        print("callback started, waiting 0.5 s.")
-        time.wait(0.5)
+        if not isinstance(channel, int):
+            channel = ps.CHANNELS[channel]
+        psg._lowLevelSetDataBuffer(channel, data, 0, 0)
+        psg._lowLevelGetValuesAsync(numSamples, 0, 1, 0, 0, data_ready, None)
+        print("Get values async started.")
     else:
         print("Data is not ready. RunBlock had an error.")
 
 
-def test_runBlock_async(channel="A", sample_interval=100e-9,
+def test_runBlock_async(ps, channel="A", sample_interval=100e-9,
                         sample_duration=2e-3):
     """Test running a block asynchronously."""
+    # Define psg globally for test_read_async.
+    global psg
+    psg = ps
     # Configuration of Picoscope
     ps.setChannel(channel=channel, coupling="DC", VRange=1)
     ps.memorySegments(1)
+    ps.setNoOfCaptures(1)
     ps.setResolution('12')
     i, samples, m = ps.setSamplingInterval(sample_interval, sample_duration)
     ps.setSimpleTrigger("A", threshold_V=0.1, timeout_ms=1)
 
+    # Define config globally for test_read_async.
     global config
     config = channel, samples
     # Run the block
     ps.runBlock(callback=test_read_async)
-    print("Run Block started, waiting 0.5 s.")
-    time.wait(0.5)
+    print("Run block started, waiting 1 s.")
+    time.sleep(1)
+    print("Run block finished.")
 
 
 if __name__ == "__main__":
@@ -147,10 +178,10 @@ if __name__ == "__main__":
 
     try:
         # Run tests.
-        test_general_unit_calls()
-        test_deviceResolution()
-        test_rapid_block_mode()
-        test_runBlock_async()
+        test_general_unit_calls(ps)
+        test_deviceResolution(ps)
+        test_rapid_block_mode(ps)
+        test_runBlock_async(ps)
     finally:
         # Close the connection
         ps.close()
