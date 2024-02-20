5c09ffde8544ee5298659aa1ec29513e8c29f77a
NINI1988 <nini16041988-github@yahoo.de>
2024-02-20 16:04:35 +0000

    Remove unused warning package

--- picoscope/ps4000a.py
+++ picoscope/ps4000a.py
@@ -63,8 +63,6 @@ from ctypes import byref, POINTER, create_string_buffer, c_float, c_double, \
     CFUNCTYPE
 from ctypes import c_int32 as c_enum
 
-import warnings
-
 from picoscope.picobase import _PicoscopeBase
 
 
