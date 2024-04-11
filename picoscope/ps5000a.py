5be00bc24c8b0bd8eca8eb26c5ef1d4ee6abee95
Mark Harfouche <mark.harfouche@gmail.com>
2024-04-11 10:04:00 -0400

    Allow USB2.0 to be used with ps5000a

--- picoscope/ps5000a.py
+++ picoscope/ps5000a.py
@@ -177,6 +177,7 @@ class PS5000a(_PicoscopeBase):
         # since the user should address this immediately, and we
         # shouldn't let this go as a soft error
         # but I think this should do for now
+        # PICO_POWER_SUPPLY_NOT_CONNECTED or PICO_USB3_0_DEVICE_NON_USB3_0_PORT
         if m == 0x11A or m == 0x11E:
             self.changePowerSource(m)
         else:
